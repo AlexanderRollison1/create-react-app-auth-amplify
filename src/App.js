8d410e2a0b558cd585779588bf745a911b492dd9
Alexander Rollison <alexander.rollison@mavs.uta.edu>
2022-05-01 06:42:42 -0500

    adding change

--- src/App.js
+++ src/App.js
@@ -11,20 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <AmplifySignOut />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Home</h1>
       </div>
     );
   }
